f9c2753e34907323a7c98da9ca0cf79930630257
gtnoble <garretnoble@gmail.com>
2024-08-01 09:18:28 -0500

    Reworked architecture to allow for hyperfunctions and hypervectors, added a hyper-map function

--- hyperreal.scm
+++ hyperreal.scm
@@ -1,217 +1,315 @@
-(import srfi-1 srfi-133)
+(import srfi-1 srfi-132 srfi-133 srfi-69 binary-search)
 
-;; Access the nth element of a hyperreal number
-(define (hyper-ref x n)
-  (if (procedure? x) (x n) x))
+(define-record hyper ultraproduct)
 
-(define (memoize func)
-  (let ((previous-x '())
-        (previous-y '()))
-    (lambda (x) (if (equal? previous-x x) 
-                    previous-y
-                    (begin
-                      (set! previous-x x)
-                      (set! previous-y (func x))
-                      previous-y)))))
+(define (is-hyper? x)
+  (or (boolean? x) (number? x) (procedure? x) (hyper? x)))
 
-(define (hyper-operation-2 operator)
-  (lambda (x y) 
-      (if (or (procedure? x) (procedure? y)) 
+
+;; Access the nth element of a hyper number
+(define (hyper-ref x i)
+  (assert (is-hyper? x))
+  (if (hyper? x)
+      ((hyper-ultraproduct x) i)
+      x))
+
+
+(define (memoize func #!key (store-all #f))
+  (assert (procedure? func))
+  (if store-all 
+      (let ((lookup-table (make-hash-table)))
+       (lambda (#!rest args)
+         (if (hash-table-exists? lookup-table args)
+             (hash-table-ref lookup-table args)
+             (let ((y (apply func args))) 
+              (hash-table-set! lookup-table x y)
+              y)))) 
+      (let ((previous-args '())
+            (previous-y '()))
+        (lambda (#!rest args) 
+          (if (equal? previous-args args) 
+              previous-y
+              (begin
+                (set! previous-args args)
+                (set! previous-y (apply func args))
+                previous-y))))))
+
+(define (functionize x)
+  (if (procedure? x)
+      x
+      (lambda (#!rest args) x)))
+
+(define (function-operation-2 operator)
+  (lambda (f1 f2)
+    (lambda (#!rest args) (operator (apply f1 args) (apply f2 args)))))
+
+(define (vector-operation-2 scalar-operator)
+  (lambda (x y)
+    (assert (and (vector? x) (vector? y)))
+    (vector-map
+      scalar-operator
+      x
+      y)))
+
+(define (operation-2 operator)
+  (let* ((scalar-operation 
+          (lambda (x y)
+            (assert (or (number? x) (procedure? x)))
+            (assert (or (number? y) (procedure? y)))
+            (if (or (procedure? x) (procedure? y))
+                (let ((f1 (functionize x))
+                      (f2 (functionize y)))
+                  (lambda (#!rest args) (operator (apply f1 args) (apply f2 args))))
+                (operator x y))))
+        (vector-operation (vector-operation-2 scalar-operation)))
+    (lambda (x y)
+      (if (and (vector? x) (vector? y))
+          (vector-operation x y)
+          (scalar-operation x y)))))
+
+(define (hyper-operation-2 number-operator)
+  (let ((operator (operation-2 number-operator))) 
+   (lambda (x y) 
+    (if (or (hyper? x) 
+            (hyper? y)) 
+        (make-hyper 
           (memoize 
-            (lambda (n) (operator (hyper-ref x n) (hyper-ref y n))))   
-          (operator x y))))
+            (lambda (i) 
+              (assert (integer? i))
+              (operator (hyper-ref x i) 
+                        (hyper-ref y i)))))
+        (operator x y)))))
 
-(define (hyper-operation-chained pairwise-operator)
-  (let ((pairwise-operation (hyper-operation-2 pairwise-operator))) 
-   (letrec ((chained-operation
-              (lambda (first-arg second-arg . remaining-args)
-                (let ((accumulated-result (pairwise-operation first-arg second-arg)))
-                 (if (null? remaining-args) 
-                     accumulated-result
-                     (apply chained-operation accumulated-result remaining-args))))))
-     chained-operation)))
+(define (operation-chained pairwise-operation)
+  (assert (procedure? pairwise-operation))
+  (letrec ((chained-operation
+             (lambda (first-arg second-arg . remaining-args)
+               (let ((accumulated-result (pairwise-operation first-arg second-arg)))
+                (if (null? remaining-args) 
+                    accumulated-result
+                    (apply chained-operation accumulated-result remaining-args))))))
+    chained-operation))
 
-;; Create a hyperreal number representing a sequence like 1/n
-(define (hyper-operation-1 operator)
+(define (chained-hyper-operation operator)
+  (operation-chained (hyper-operation-2 operator)))
+
+(define add-hyper (chained-hyper-operation +))
+(define subtract-hyper (chained-hyper-operation -))
+(define multiply-hyper (chained-hyper-operation *))
+(define divide-hyper (chained-hyper-operation /))
+
+(define (vector-operation-1 scalar-operator)
   (lambda (x) 
-      (if (procedure? x) 
-          (memoize
-            (lambda (n) (operator (hyper-ref x n))))
-          (operator x))))
+    (assert (vector? x))
+    (vector-map scalar-operator x)))
+
+(define (operation-1 operator)
+  (let* ((scalar-operation 
+           (lambda (x)
+             (if (procedure? x)
+                 (lambda (#!rest args) (operator (apply x args)))
+                 (operator x))))
+         (vector-operation (vector-operation-1 scalar-operation)))
+    (lambda (x)
+      (if (vector? x)
+          (vector-operation x)
+          (scalar-operation x)))))
+
+(define (hyper-operation-1 operator)
+  (let ((operation (operation-1 operator))) 
+   (lambda (x) 
+     (if (hyper? x) 
+         (make-hyper 
+           (memoize
+             (lambda (i) 
+               (operation (hyper-ref x i)))))
+         (operation x)))))
 
 (define-syntax hyper-if 
   (syntax-rules ()
     ((_ condition on-true on-false) 
      (let ((bound-condition condition)) 
-      (check-boolean-or-hyperboolean bound-condition)
-      (memoize 
-        (lambda (i) (if (hyper-ref bound-condition i) 
-                        (hyper-ref on-true i)
-                        (hyper-ref on-false i))))))))
+      (if (hyper? bound-condition) 
+          (make-hyper 
+        (memoize 
+          (lambda (i)  
+            (if (procedure? bound-condition) 
+                (lambda (#!rest args) 
+                  (if (apply (hyper-ref bound-condition i) args)
+                      (apply (functionize (hyper-ref on-true i)) 
+                             args)
+                      (apply (functionize (hyper-ref on-false i)) 
+                             args)))
+                (if (hyper-ref bound-condition i)
+                    (hyper-ref on-true i)
+                    (hyper-ref on-false i))))))
+          (if bound-condition on-true on-false))))))
 
-(define (index-hyperreal)
-  (lambda (x) (+ x 1)))
+(define (index-hyper)
+  (make-hyper (lambda (i) (+ i 1))))
 
-(define (vector-operation-1 operator)
-  (lambda (input-vector)
-    (vector-map operator input-vector)))
+;; Addition of two hyper numbers
+(define (scalar-multiply-vector scalar vect)
+  (vector-map (lambda (element) (multiply-hyper scalar element)) vect))
 
-(define (vector-fill reference-vector filler)
-  (if (not (vector? filler))
-      (make-vector 
-        (vector-length reference-vector) 
-        filler)
-      filler))
+(define expt-hyper (hyper-operation-2 expt))
 
-(define (vector-or-scalar-operation-2 scalar-operator)
-  (lambda (x y)
-    (if (or (vector? x) (vector? y))
-        (let ((vector-1 (vector-fill x y))
-              (vector-2 (vector-fill y x))) 
-          (vector-map
-            (lambda (element-1 element-2) 
-              (scalar-operator element-1 element-2))
-            vector-1
-            vector-2))
-        (scalar-operator x y))))
+(define lte-hyper (hyper-operation-2 <=))
+(define gte-hyper (hyper-operation-2 >=))
+(define gt-hyper (hyper-operation-2 >))
+(define eq-hyper (hyper-operation-2 =))
 
-(define (vector-or-scalar-operation-1 scalar-operator)
-  (lambda (x) (if (vector? x)
-                  (vector-map scalar-operator x)
-                  (scalar-operator x))))
+(define (reciprocal-hyper x) 
+  (divide-hyper 1 x))
+(define exp-hyper (hyper-operation-1 exp))
+(define negate-hyper (hyper-operation-1 -))
+(define sqrt-hyper (hyper-operation-1 sqrt))
+(define square-hyper (hyper-operation-1 (lambda (x) (expt x 2))))
 
-(define (scalar-vector-operation operation)
-  (lambda (input-vector scalar)
-    (vector-map
-      (lambda (element) (operation element))
-      input-vector)))
+(define (sum-vector-elements vect) 
+  (assert (vector? vect))
+  (vector-fold add-hyper 0 x))
 
-(define (scalar-or-vector-operation-chained scalar-operator)
-  (hyper-operation-chained 
-                        (vector-or-scalar-operation-2 scalar-operator)))
+(define (vector-dot vector-1 vector-2)
+  (assert (vector? vector-1))
+  (assert (vector? vector-2))
+  (sum-vector-elements 
+    (hadamard-multiply-vector vector-1 vector-2)))
 
-;; Addition of two hyperreal numbers
-(define add-hyperreal (scalar-or-vector-operation-chained +))
-(define sub-hyperreal (scalar-or-vector-operation-chained -))
-(define mul-hyperreal (scalar-or-vector-operation-chained *))
-(define div-hyperreal (scalar-or-vector-operation-chained /))
+(define (hyper-vector-norm-squared vect)
+  (assert (vector? vect))
+  (vector-dot vect vect))
 
-(define (hyper-scalar-or-vector-operation-2 scalar-operator)
-  (hyper-operation-2
-    (vector-or-scalar-operation-2 scalar-operator)))
-
-(define expt-hyperreal (hyper-scalar-or-vector-operation-2 expt))
-
-(define lte-hyperreal (hyper-operation-2 <=))
-(define gte-hyperreal (hyper-operation-2 >=))
-(define gt-hyperreal (hyper-operation-2 >))
-
-(define (hyper-scalar-or-vector-operation-1 scalar-operator)
-  (hyper-operation-1 
-    (vector-or-scalar-operation-1 
-      scalar-operator)))
-
-(define reciprocal-hyperreal 
-  (hyper-scalar-or-vector-operation-1
-    (lambda (x) (/ 1 x))))
-(define exp-hyperreal (hyper-scalar-or-vector-operation-1 exp))
-(define neg-hyperreal (hyper-scalar-or-vector-operation-1 -))
-(define sqrt-hyperreal (hyper-scalar-or-vector-operation-1 sqrt))
-(define square-hyperreal (hyper-scalar-or-vector-operation-1 (lambda (x) (expt x 2))))
-
-(define hyper-vector-ref (hyper-operation-2 vector-ref))
-
-(define sum (hyper-operation-1 
-              (lambda (x) (if (vector? x)
-                              (vector-fold add-hyperreal 0 x)
-                              x))))
-
-(define (vector-norm-squared vect)
-  (sum (square-hyperreal vect)))
-
-(define (vector-norm vect)
-  (sqrt-hyperreal (vector-norm-squared vect)))
+(define (hyper-vector-norm vect)
+  (assert (vector? vect))
+  (sqrt-hyper (hyper-vector-norm-squared vect)))
 
 (define (normalize-hyper-vector hyper-vector)
-  (mul-hyperreal 
-    (div-hyperreal hyper-vector 
-                   (hyper-vector-norm hyper-vector))))
-
-(define (check-function func)
-  (if (not (procedure? func)) 
-      (error "Not a function" func)))
+  (assert (vector? vect))
+  (divide-hyper hyper-vector 
+                 (hyper-vector-norm hyper-vector)))
 
 (define (aitken-extrapolate func)
-  (check-function func)
+  (assert (procedure? func))
   (lambda (n) 
     (let* ((xn (func n))
-           (xn+1 (func (+ n 1)))
-           (xn+2 (func (+ n 2)))
-           (delta-xn (- xn+1 xn))
-           (delta-squared-xn (+ xn (- (* 2 xn+1)) xn+2))
-           )
-      (if (= 0 delta-squared-xn) 
-          xn
-          (- xn (/ (expt delta-xn 2) delta-squared-xn))))))
+           (xn+1 (func (add-hyper n 1)))
+           (xn+2 (func (add-hyper n 2)))
+           (delta-xn (subtract-hyper xn+1 xn))
+           (delta-squared-xn (add-hyper xn 
+                                        (negate-hyper (multiply-hyper 2 xn+1)) 
+                                        xn+2)))
+      (hyper-if (eq-hyper 0 delta-squared-xn) 
+                xn
+                (subtract-hyper xn 
+                                (divide-hyper (expt-hyper delta-xn 2) 
+                                              delta-squared-xn))))))
 
 (define (standard-part h #!optional (n 1000) #!key (extrapolate? #t))
-  (if (procedure? h) 
+  (assert (integer? n))
+  (if (hyper? h) 
       (if extrapolate? 
           ((aitken-extrapolate (lambda (n) (hyper-ref h n))) 
            n)
           (hyper-ref h n))
       h))
 
-;; Create an infinitesimal hyperreal number representing the sequence 1/n
+;; Create an infinitesimal hyper number representing the sequence 1/n
 (define infinitesimal
-  (div-hyperreal 1 (expt-hyperreal 2 (index-hyperreal))))
+  (divide-hyper 1 (expt-hyper 2 (index-hyper))))
 
 (define negative-infinitesimal
-  (neg-hyperreal infinitesimal))
+  (negate-hyper infinitesimal))
 
-;; Create an infinitesimal hyperreal number representing a smaller infinitesimal 1/n^2
+;; Create an infinitesimal hyper number representing a smaller infinitesimal 1/n^2
 (define infinitesimal-squared
-  (reciprocal-hyperreal (expt-hyperreal (index-hyperreal) 2)))
+  (reciprocal-hyper (expt-hyper (index-hyper) 2)))
 
-(define (differentiate f #!key (order 1) (direction 'forward))
-  (check-function f)
-  (let ((dx (if (equal? direction 'forward) 
-                infinitesimal
-                (neg-hyperreal infinitesimal))))
-    (hyper-if (lte-hyperreal order 0) 
-                  f
-                  (differentiate 
-                    (lambda (x) 
-                      (div-hyperreal 
-                        (sub-hyperreal 
-                          (f (add-hyperreal dx x)) 
-                          (f x)) 
-                        dx))
-                    order: (- order 1)))))
+(define (integrate f a b #!key (step-size infinitesimal))
+  (assert (procedure? f))
+  (hyper-map (lambda (f a b step-size) 
+               (letrec ((delta-x step-size)
+                        (do-integration 
+                          (lambda (x running-sum) 
+                            (hyper-if (gte-hyper x b) 
+                                      running-sum
+                                      (do-integration 
+                                        (add-hyper delta-x x) 
+                                        (add-hyper (multiply-hyper delta-x (f x)) 
+                                                   running-sum))))))
+                 (do-integration a 0)))
+             f
+             a
+             b
+             step-size))
 
-(define (integrate f a b)
-  (check-function f)
-  (letrec ((delta-x infinitesimal)
-           (do-integration 
-             (lambda (x running-sum) 
-               (hyper-if (gte-hyperreal x b) 
-                             running-sum
-                             (do-integration 
-                               (add-hyperreal delta-x x) 
-                               (add-hyperreal (mul-hyperreal delta-x (f x)) 
-                                              running-sum))))))
-    (do-integration a 0)))
+(define (hyper-map func #!rest hyper-vals)
+  (make-hyper 
+    (lambda (i)
+      (apply func 
+             (map 
+               (lambda (hyper) 
+                 (hyper-ref hyper i)) 
+               hyper-vals)))))
 
-(define (ode-integrate f df/dt t)
-  (check-function f)
-  (check-function df/dt)
-  (let* ((dt negative-infinitesimal)
-         (previous-t (add-hyperreal t dt))
-         (previous-y (f previous-t))
-         (dy (mul-hyperreal 
-               (df/dt t previous-y) 
-               dt)))
-    (sub-hyperreal previous-y dy)))
+(define (hyper-eval hyperfunction #!rest args)
+  (apply hyper-map (lambda (function-slice #!rest arg-slices) 
+                     (apply function-slice arg-slices)) 
+         hyperfunction args))
 
-(define (print-hyperreal h #!optional (n 10))
+(define (differentiate f #!key (direction 'forward) (step-size infinitesimal))
+  (assert (is-hyper? f))
+  (assert (is-hyper? step-size))
+  (hyper-map (lambda (fi dx) 
+               (lambda (x) 
+                 (divide-hyper 
+                   (subtract-hyper 
+                     (fi (add-hyper dx x)) 
+                     (fi x)) 
+                   dx)))
+             f (if (equal? direction 'forward) 
+                   step-size
+                   (negate-hyper step-size))))
+
+(define-record point x y)
+
+(define (make-interpolant-1d coords)
+  (let* ((adjacent-x-differences (lambda (point-1 point-2) 
+                                   (- (point-x point-1) (point-x point-2))))
+         (compare-x-values (lambda (point-1 point-2) 
+                             (<= 0 (adjacent-x-differences point-1 point-2))))
+         (ordered-coords (vector-sort compare-x-values coords)))
+    (lambda (xi)
+      (let* ((before-point (binary-search ordered-coords 
+                                          (lambda (candidate) 
+                                            (compare-x-values candidate xi))))
+             (after-point (binary-search ordered-coords 
+                                         (lambda (candidate) 
+                                           (- (compare-x-values candidate xi)))))
+             (delta-x (subtract-hyper (point-x after-point) 
+                                      (point-x before-point)))
+             (delta-y (subtract-hyper (point-y after-point) 
+                                      (point-y before-point)))
+             (slope (divide-hyper delta-y delta-x)))
+        (hyper-add (point-y before-point) 
+                   (hyper-multiply (subtract-hyper xi 
+                                                   (point-x before-point))
+                                   slope))))))
+
+(define (ode-integrate t df/dt t0 y0 #!key (step-size infinitesimal))
+  (assert (procedure? df/dt))
+  (letrec ((do-integrate 
+             (lambda (current-t current-y) 
+               (hyper-if (gte-hyper current-t t)
+                         current-y
+                         (let* ((dt step-size)
+                                (dy (multiply-hyper 
+                                      (df/dt current-t current-y) 
+                                      dt))
+                                (next-t (add-hyper current-t dt))
+                                (next-y (add-hyper current-y dy)))
+                           (do-integrate next-t next-y))))))
+    (do-integrate t0 y0)))
+
+(define (print-hyper h #!optional (n 10))
   (print (map (lambda (i) (hyper-ref h i)) (iota n))))
--- hyperreal.test.scm
+++ hyperreal.test.scm
@@ -3,10 +3,10 @@
 
 (load "hyperreal.scm")
 
-(define (real-hyperreal i)
-  69)
-(define (one-hyperreal i)
-  1)
+(define real-hyper (make-hyper (lambda (i) 69)))
+(check (hyper? real-hyper) => #t)
+(define one-hyper (make-hyper (lambda (i) 1)))
+(check (hyper? one-hyper) => #t)
 
 (define (first-element x)
   (hyper-ref x 0))
@@ -14,29 +14,42 @@
   (hyper-ref x 1))
 
 (check (first-element 69) => 69)
-(check (second-element real-hyperreal) => 69)
+(check (second-element real-hyper) => 69)
 
-(check (first-element (index-hyperreal)) => 1)
-(check (second-element (index-hyperreal)) => 2)
+(check (first-element (index-hyper)) => 1)
+(check (second-element (index-hyper)) => 2)
 
-(check (add-hyperreal 1 2) => 3)
-(define hyperreal-sum (add-hyperreal real-hyperreal real-hyperreal))
-(check (procedure? hyperreal-sum) => #t)
-(check (first-element hyperreal-sum) => (+ 69 69))
+(check (add-hyper 1 2) => 3)
+(check (add-hyper 1 2 3 4) => 10)
+(check (add-hyper #(1 2) #(3 4)) => #(4 6))
+(define hyper-sum (add-hyper real-hyper real-hyper))
+(check (hyper? hyper-sum) => #t)
+(check (first-element hyper-sum) => (+ 69 69))
 
-(check (reciprocal-hyperreal 2) => (/ 1 2))
-(define hyperreal-reciprocal (reciprocal-hyperreal real-hyperreal))
-(check (procedure? hyperreal-reciprocal) => #t)
-(check (first-element hyperreal-reciprocal) => (/ 1 69))
+(define hyper-monomial (make-hyper (lambda (i)
+                                     (lambda (x)
+                                       (expt x i)))))
+
+(check ((hyper-ref hyper-monomial 1) 2) => 2)
+(check ((hyper-ref hyper-monomial 2) 2) => 4)
+
+(check (reciprocal-hyper 2) => (/ 1 2))
+(define hyper-reciprocal (reciprocal-hyper real-hyper))
+(check (hyper? hyper-reciprocal) => #t)
+(check (first-element hyper-reciprocal) => (/ 1 69))
 
 (define (near? x y #!optional (tol 0.001))
   (if (< (abs (- x y)) tol)
       #t
       #f))
 
-(check (near? ((aitken-extrapolate infinitesimal) 10) 0) => #t)
+(check (near? ((aitken-extrapolate (hyper-ultraproduct infinitesimal)) 
+                10) 
+              0) => #t)
 (check (near? (standard-part infinitesimal) 
               0) => #t)
+
+(check ((standard-part hyper-monomial) 1) => 1)
 
 (define (limited-standard-part x) 
   (standard-part x 10))
@@ -44,45 +57,40 @@
 (define (linear-function x)
   x)
 (define linear-derivative (differentiate linear-function))
-(check (standard-part
-         (linear-derivative 1)) => 1)
-(check (standard-part (linear-derivative one-hyperreal)) => 1)
-(check (standard-part (linear-derivative real-hyperreal)) => 1)
+(check ((standard-part
+         linear-derivative) 1) => 1)
+(check ((standard-part
+         linear-derivative) 2) => 1)
+(check (standard-part (hyper-eval linear-derivative one-hyper)) => 1)
+(check (standard-part (hyper-eval linear-derivative real-hyper)) => 1)
 (check (limited-standard-part (integrate linear-function 0 1)) => (/ 1 2))
 
 (define (parabolic-function x)
-  (expt-hyperreal x 2))
+  (expt-hyper x 2))
+(check (parabolic-function 2) => 4)
 (define parabolic-derivative (differentiate parabolic-function))
-(check (standard-part (parabolic-derivative 1)) => 2)
-(check (standard-part (parabolic-derivative one-hyperreal)) => 2)
+(check (near? ((standard-part parabolic-derivative) 1) 2) => #t)
+(check (near? (standard-part (hyper-eval parabolic-derivative one-hyper)) 2) => #t)
 (check (near? (limited-standard-part 
                 (integrate parabolic-function 0 1)) 
               (/ 1 3)) => #t)
 (check (near? (limited-standard-part 
-                (integrate parabolic-function 0 one-hyperreal)) 
+                (integrate parabolic-function 0 one-hyper)) 
               (/ 1 3)) => #t)
 
 (define decay-constant -1)
 
 (define (exponential-decay t)
-  (memoize 
-    (if-hyperreal (lte-hyperreal t 0)
-                  1
-                  (ode-integrate exponential-decay 
-                                 (lambda (t y) (mul-hyperreal y decay-constant))
-                                 t))))
+    (ode-integrate t 
+                   (lambda (t y) 
+                     (multiply-hyper decay-constant y))
+                   0
+                   0))
 
 (check (near? (standard-part (exponential-decay 10) 3 extrapolate?: #t) 0) 
               => #t)
 
 (define cannonball-initial-position #(0 0))
-(define (cannonball-trajectory t)
-  (if-hyperreal (lte-hyperreal t 0)
-                cannonball-initial-position
-                (ode-integrate cannonball-trajectory
-                               cannonball-velocity
-                               t)))
-
 (define muzzle-speed 10)
 (define pi (acos -1))
 (define firing-angle (/ pi 4))
@@ -92,25 +100,31 @@
           (* muzzle-speed
              (sin firing-angle))))
 (define cannonball-mass 1)
-
-(define (cannonball-velocity t y)
-  (if-hyperreal (lte-hyperreal t 0)
-                cannonball-initial-velocity
-                (ode-integrate cannonball-velocity
-                               cannonball-acceleration
-                               t)))
-
 (define drag-coefficient 1)
+
+(define (cannonball-trajectory t)
+  (ode-integrate t
+                 cannonball-velocity
+                 0
+                 cannonball-initial-position))
+
+(define (cannonball-velocity t position)
+  ;(print (hyper-ref position 10))
+  (ode-integrate t
+                 cannonball-acceleration
+                 0
+                 cannonball-initial-velocity))
+
 (define (cannonball-acceleration t velocity)
-  (let* ((cannonball-speed (vector-norm-hyperreal y))
-        (drag-force (mul-scalar-vector-hyperreal 
-                      (mul-hyperreal -1
-                                      drag-coefficient
-                                      (square-hyperreal (vector-norm-hyperreal velocity)))
-                      (normalize-vector-hyperreal velocity)))
-        (drag-acceleration (mul-scalar-vector-hyperreal 
-                             (hyperreal-reciprocal cannonball-mass) 
-                             drag-force))
-        (gravitational-acceleration (vector-hyperreal 0 -9.8)))
-    ()
-    ))
+  (let* ((drag-force (mul-hyper 
+                       -1
+                       drag-coefficient
+                       (hyper-vector-norm-squared velocity)
+                       (normalize-hyper-vector velocity)))
+         (drag-acceleration (div-hyper 
+                              drag-force
+                              cannonball-mass))
+         (gravitational-acceleration (vector 0 -9.8)))
+    (add-hyper drag-force gravitational-acceleration)))
+
+(print-hyper (standard-part (cannonball-trajectory 0.1) 10 extrapolate?: #f))
